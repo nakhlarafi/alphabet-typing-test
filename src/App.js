3ca260958d12e9cb24d8636fbeef3ac039eb9a4c
nakhlarafi <nakhla.rafi@g.bracu.ac.bd>
2021-08-03 03:08:33 +0600

    Next Char Notification added.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Alert from "./Alert";
 
 function App() {
   const [name, setName] = useState("");
+  const [charNext, setCharNext] = useState("a");
   const [isEditing, setIsEditing] = useState(false);
 
   const handleSubmit = (e) => {
@@ -31,7 +32,7 @@ function App() {
     if (isActive) {
       interval = setInterval(() => {
         setSeconds((second) => second + 1);
-      }, 10);
+      }, 1);
     } else if (!isActive && seconds !== 0) {
       clearInterval(interval);
     }
@@ -40,85 +41,111 @@ function App() {
 
   const checkChar = (e) => {
     let charWritten = e.target.value;
-    if (name == "" && charWritten == "a") {
+    if (name === "" && charWritten === "a") {
       setName(charWritten);
       setIsEditing(true);
       setIsActive(true);
+      setCharNext("b");
     }
-    if (name == "a" && charWritten.slice(-1) == "b") {
+    if (name === "a" && charWritten.slice(-1) === "b") {
       setName(charWritten);
+      setCharNext("c");
     }
     if (name.slice(-1) === "b" && charWritten.slice(-1) === "c") {
       setName(charWritten);
+      setCharNext("d");
     }
     if (name.slice(-1) === "c" && charWritten.slice(-1) === "d") {
       setName(charWritten);
+      setCharNext("e");
     }
     if (name.slice(-1) === "d" && charWritten.slice(-1) === "e") {
       setName(charWritten);
+      setCharNext("f");
     }
     if (name.slice(-1) === "e" && charWritten.slice(-1) === "f") {
       setName(charWritten);
+      setCharNext("g");
     }
     if (name.slice(-1) === "f" && charWritten.slice(-1) === "g") {
       setName(charWritten);
+      setCharNext("h");
     }
     if (name.slice(-1) === "g" && charWritten.slice(-1) === "h") {
       setName(charWritten);
+      setCharNext("i");
     }
     if (name.slice(-1) === "h" && charWritten.slice(-1) === "i") {
       setName(charWritten);
+      setCharNext("j");
     }
     if (name.slice(-1) === "i" && charWritten.slice(-1) === "j") {
       setName(charWritten);
+      setCharNext("k");
     }
     if (name.slice(-1) === "j" && charWritten.slice(-1) === "k") {
       setName(charWritten);
+      setCharNext("l");
     }
     if (name.slice(-1) === "k" && charWritten.slice(-1) === "l") {
       setName(charWritten);
+      setCharNext("m");
     }
     if (name.slice(-1) === "l" && charWritten.slice(-1) === "m") {
       setName(charWritten);
+      setCharNext("n");
     }
     if (name.slice(-1) === "m" && charWritten.slice(-1) === "n") {
       setName(charWritten);
+      setCharNext("o");
     }
     if (name.slice(-1) === "n" && charWritten.slice(-1) === "o") {
       setName(charWritten);
+      setCharNext("p");
     }
     if (name.slice(-1) === "o" && charWritten.slice(-1) === "p") {
       setName(charWritten);
+      setCharNext("q");
     }
     if (name.slice(-1) === "p" && charWritten.slice(-1) === "q") {
       setName(charWritten);
+      setCharNext("r");
     }
     if (name.slice(-1) === "q" && charWritten.slice(-1) === "r") {
       setName(charWritten);
+      setCharNext("s");
     }
     if (name.slice(-1) === "r" && charWritten.slice(-1) === "s") {
       setName(charWritten);
+      setCharNext("t");
     }
     if (name.slice(-1) === "s" && charWritten.slice(-1) === "t") {
       setName(charWritten);
+      setCharNext("u");
     }
     if (name.slice(-1) === "t" && charWritten.slice(-1) === "u") {
       setName(charWritten);
+      setCharNext("v");
     }
     if (name.slice(-1) === "u" && charWritten.slice(-1) === "v") {
       setName(charWritten);
+      setCharNext("w");
     }
     if (name.slice(-1) === "v" && charWritten.slice(-1) === "w") {
       setName(charWritten);
+      setCharNext("x");
     }
     if (name.slice(-1) === "w" && charWritten.slice(-1) === "x") {
       setName(charWritten);
+      setCharNext("y");
     }
     if (name.slice(-1) === "x" && charWritten.slice(-1) === "y") {
       setName(charWritten);
+      setCharNext("z");
     }
     if (name.slice(-1) === "y" && charWritten.slice(-1) === "z") {
       setName(charWritten);
+      setCharNext("Finished!");
       setIsEditing(false);
       setIsActive(false);
     }
@@ -128,7 +155,8 @@ function App() {
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        <h3>grocery bud</h3>
+        <h3>Type the Alphabet</h3>
+        <h3 className="timer">{charNext}</h3>
         <div className="form-control">
           <input
             type="text"
